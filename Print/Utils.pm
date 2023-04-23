d63cfd09b99b6a28984da6f475357bd0f0c661ca
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-04-23 17:07:48 +0200

    Add doc example in Utils

--- Print/Utils.pm
+++ Print/Utils.pm
@@ -246,7 +246,7 @@ Returns array with pretty print strings.
          From print_common():
                  Multiple values are printed to one line.
 
-=head1 EXAMPLE
+=head1 EXAMPLE1
 
 =for comment filename=utils_print_aliases.pl
 
@@ -269,6 +269,29 @@ Returns array with pretty print strings.
  # Output:
  # Aliases:
  #   pes domácí (cs)
+
+=head1 EXAMPLE2
+
+=for comment filename=utils_print_descriptions.pl
+
+ use strict;
+ use warnings;
+
+ use Unicode::UTF8 qw(encode_utf8);
+ use Test::Shared::Fixture::Wikibase::Datatype::Item::Wikidata::Dog;
+ use Wikibase::Datatype::Print::Utils qw(print_descriptions);
+ use Wikibase::Datatype::Print::Value::Monolingual;
+
+ my $obj = Test::Shared::Fixture::Wikibase::Datatype::Item::Wikidata::Dog->new;
+ my @ret = print_descriptions($obj, {'lang' => 'cs'},
+         \&Wikibase::Datatype::Print::Value::Monolingual::print);
+
+ # Print.
+ print encode_utf8(join "\n", @ret);
+ print "\n";
+
+ # Output:
+ # Description: domácí zvíře (cs)
 
 =head1 DEPENDENCIES
 
