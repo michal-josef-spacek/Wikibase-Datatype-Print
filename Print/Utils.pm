c405b78674fddc824d3aaf6ed433641393e970ef
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-02 09:29:00 +0200

    Add example for print_forms() to Util

--- Print/Utils.pm
+++ Print/Utils.pm
@@ -293,6 +293,34 @@ Returns array with pretty print strings.
  # Output:
  # Description: domácí zvíře (cs)
 
+=head1 EXAMPLE3
+
+=for comment filename=utils_print_forms.pl
+
+ use strict;
+ use warnings;
+
+ use Unicode::UTF8 qw(encode_utf8);
+ use Test::Shared::Fixture::Wikibase::Datatype::Lexeme::Wikidata::DogCzechNoun;
+ use Wikibase::Datatype::Print::Form;
+ use Wikibase::Datatype::Print::Utils qw(print_forms);
+
+ my $obj = Test::Shared::Fixture::Wikibase::Datatype::Lexeme::Wikidata::DogCzechNoun->new;
+ my @ret = print_forms($obj, {'lang' => 'cs'},
+         \&Wikibase::Datatype::Print::Form::print);
+
+ # Print.
+ print encode_utf8(join "\n", @ret);
+ print "\n";
+
+ # Output:
+ # Forms:
+ #   Id: L469-F1
+ #   Representation: pes (cs)
+ #   Grammatical features: Q110786, Q131105
+ #   Statements:
+ #     P898: pɛs (normal)
+
 =head1 DEPENDENCIES
 
 L<Error::Pure>,
