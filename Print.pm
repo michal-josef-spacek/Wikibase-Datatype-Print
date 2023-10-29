b4b8f77590091c8fdeb0eb9b0bb9dd2d817c642c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-10-29 16:45:18 +0100

    Prepare for 0.16 version

--- Print.pm
+++ Print.pm
@@ -3,7 +3,7 @@ package Wikibase::Datatype::Print;
 use strict;
 use warnings;
 
-our $VERSION = 0.15;
+our $VERSION = 0.16;
 
 1;
 
@@ -147,6 +147,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.15
+0.16
 
 =cut
